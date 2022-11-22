75d00e0a4d231a067f58005ba27e71c3dfe0b0ae
Xoloshef <xoloshef@gmail.com>
2022-11-23 00:27:09 +0300

    develop views + urls

--- maze_bank/maze_bank/urls.py
+++ maze_bank/maze_bank/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from mazebank.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('', index),   #http://.../
+    #path('cats/', categories),
+    path('', include('mazebank.urls'))
 ]
+
