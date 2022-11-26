6dae5fc7190d03ff194c3b877c20d9250864d20c
Xoloshef <xoloshef@gmail.com>
2022-11-26 18:28:16 +0300

    develop (0.6.1) удаленно не нужное + активировали css и static

--- maze_bank/maze_bank/settings.py
+++ maze_bank/maze_bank/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # в режиме эксплуатации
+STATICFILES_DIRS = [] # в случае нестандартных путей
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
