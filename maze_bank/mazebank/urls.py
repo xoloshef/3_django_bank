792f9489a5feea2a8eada5c35067d7829ebda799
Andrey Xoloshef <xoloshef@gmail.com>
2022-11-29 17:08:49 +0300

    (0.6.5) add frontend client + workers 40% realizete

--- maze_bank/mazebank/urls.py
+++ maze_bank/mazebank/urls.py
@@ -6,10 +6,21 @@ urlpatterns = [
     path('', index, name='home'),
     path('index', index, name='home'),
     path('index.html', index, name='home'),
-    path('more/index.html', index, name='home'),
+    path('login/index', index, name='home'),
     path('about/', about, name='about'),
-    path('more/login.html/', login, name='login'),
-    path('more/register.html/', register, name='register'),
+    path('login/', login, name='login'),
+    path('login.html', login, name='login'),
+    path('register/', register, name='register'),
+    path('register.html/', register, name='register'),
+    path('dontpassword', dontpassword, name='dontpassword'),
+    path('login/dontpassword.html/', dontpassword, name='dontpassword'),
+
+    path('client_index/', client_index, name='client_index'),
+
+
+
+
+
     path('about/', about, name='about'),
     path('about/', about, name='about'),
     path('about/', about, name='about'),
